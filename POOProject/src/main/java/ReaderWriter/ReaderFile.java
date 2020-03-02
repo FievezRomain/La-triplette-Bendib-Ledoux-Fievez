c3235fb8f7faf0984c8c0e5b02e3bdcbfa193287
Louis Ledoux <ledoux.louis.91@gmail.com>
2020-03-02 17:23:54 +0100

    Interface ReaderFile

---
+++ POOProject/src/main/java/ReaderWriter/ReaderFile.java
@@ -0,0 +1,39 @@
+package ReaderWriter;
+
+import java.io.IOException;
+
+import javax.swing.JPopupMenu.Separator;
+
+import Metadata.Table;
+
+public interface ReaderFile {
+	/**
+	 * Lecture du fichier contenant les valeurs principales
+	 * @param separator
+	 * @param path
+	 * @return table avec les données du fichier qui comporte les données principales
+	 */
+	public Table readInput(Separator separator, String path) throws IOException;
+	
+	/**
+	 * Lecture du fichier contenant les règles à appliquées
+	 * Retourne la table contenant les valeurs principales en ajoutant des caractéristiques dans 
+	 * la structure de données
+	 * @param separator
+	 * @param path
+	 * @param datasInput
+	 * @return 
+	 */
+	public Table readRules(Separator separator, String path, Table datasInput) throws IOException;
+	
+	/**
+	 * Lecture du fhicher contenant la description des valeurs principales
+	 * Retourne la table contenant les valeurs principales en ajoutant des caractéristiques dans
+	 * la structure de données
+	 * @param separator
+	 * @param path
+	 * @param datasInput
+	 * @return
+	 */
+	public Table readTypes(Separator separator, String path, Table datasInput) throws IOException;
+}
